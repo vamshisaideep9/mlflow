a2f8ec199d0affd48a4e9905f3424b94d53a9f4c
vamshisaideep9 <vamshisaideep@gmail.com>
2026-04-10 10:23:04 +0530

    removed files

--- DVC/src/data_ingestion.py
+++ DVC/src/data_ingestion.py
@@ -79,7 +79,7 @@ def save_data(train_data: pd.DataFrame, test_data: pd.DataFrame, data_path: str)
 
 def main():
     try:
-        test_size = 0.2
+        test_size = 0.21
         data_path = 'C:/Users/vamsh/OneDrive/Desktop/ml flow/mlflow/DVC/experiments/spam.csv'
         df = load_data(data_url=data_path)
         final_df = preprocess_data(df)
