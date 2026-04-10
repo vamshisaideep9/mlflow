8cff456f1a7cd59375c5bb33f86c0083888b2711
vamshisaideep9 <vamshisaideep@gmail.com>
2026-04-10 10:58:36 +0530

    params configured for the pipeline

--- DVC/src/data_ingestion.py
+++ DVC/src/data_ingestion.py
@@ -1,4 +1,5 @@
 import os
+import yaml
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import logging
@@ -32,6 +33,24 @@ logger.addHandler(file_handler)
 
 
 
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        raise
+
+    
 def load_data(data_url: str) -> pd.DataFrame:
     """Load data from a CSV file."""
     try:
@@ -79,7 +98,8 @@ def save_data(train_data: pd.DataFrame, test_data: pd.DataFrame, data_path: str)
 
 def main():
     try:
-        test_size = 0.21
+        params = load_params(params_path='C:/Users/vamsh/ml flow/mlflow/DVC/params.yaml')
+        test_size = params['data_ingestion']['test_size']
         data_path = 'C:/Users/vamsh/ml flow/mlflow/DVC/experiments/spam.csv'
         df = load_data(data_url=data_path)
         final_df = preprocess_data(df)
--- DVC/src/model_evaluation.py
+++ DVC/src/model_evaluation.py
@@ -32,6 +32,24 @@ logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
 
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        raise
+
+
 def load_model(file_path: str):
     """Load the trained model from a file."""
     try:
@@ -100,6 +118,7 @@ def save_metrics(metrics: dict, file_path:str) -> None:
 
 def main():
     try:
+        params = load_params(params_path='params.yaml')
         clf = load_model('src/models/model.pkl')
         test_data = load_data('src/data/processed/test_tfidf.csv')
 
@@ -114,7 +133,7 @@ def main():
             live.log_metric('accuracy', accuracy_score(y_test, y_test))
             live.log_metric('precision', precision_score(y_test, y_test))
             live.log_metric('recall', recall_score(y_test, y_test))
-            # live.log_params(params)
+            live.log_params(params)
         save_metrics(metrics, 'src/reports/metrics.json')
     except Exception as e:
         logger.error('Failed to complete the model evaluation process: %s', e)
